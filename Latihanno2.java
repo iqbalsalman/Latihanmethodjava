c3bc2e9c8fe3be211df5b19af25cc06ece6e7939
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-05 10:13:20 +0700

    Latihan Method java

---
+++ Latihanno2.java
@@ -0,0 +1,13 @@
+public class Latihanno2{
+String nama;
+	public void setNamaLengkap(String nama) {
+        this.nama = nama;
+    }
+    public static void main(String[] args){
+      Latihanno3 boss = new Latihanno3();
+        boss.getNamaLengkap();
+        boss.setNamaLengkap(1);
+    	System.out.println("Nama Saya ",getNamaLengkap());
+    }
+ 
+}///masih error
---
+++ Latihanno3.java
@@ -0,0 +1,8 @@
+public class Latihanno3{
+	  String nama;
+
+public String getNamaLengkap(){
+    return nama;
+  }
+
+}
